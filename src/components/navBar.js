d0ad11bd88af9372b4095549b94cb2bf0d38b9c6
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-02-27 15:20:31 +0100

    Starting Nav-Bar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,5 +1,21 @@
-import React from "react";
+import Link from "next/link";
 
-export default function NavBar() {
-  return <div></div>;
-}
+const textStyle = "hover:text-amber-400"; //Tailwind style
+
+const Navbar = () => {
+  return (
+    <div class="flex flex-row w-full bg-[#cdcdcd] items-center justify-around">
+      <nav class="p-4 text-white">
+        <ul class="flex gap-14 text-xl list-none m-0 p-0">
+          <li class={textStyle}>
+            <Link href="/">The Quiz</Link>
+          </li>
+          <li class={textStyle}>
+            <Link href="/adminPage">Admin</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/adminPage.css";
 import "@/styles/quizPage.css";
+import Navbar from "@/components/NavBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;{" "}
+    </>
+  );
 }
