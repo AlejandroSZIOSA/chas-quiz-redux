38657259aac6e10b5bdc109b77e2ea6eba89f9a2
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-02-27 14:48:18 +0100

    Initial project structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/adminPage.css";
+import "@/styles/quizPage.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ src/pages/adminPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <main>
+      <h1>Admin Page</h1>
+    </main>
+  );
+}
