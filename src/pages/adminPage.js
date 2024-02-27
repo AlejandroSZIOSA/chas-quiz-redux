95d3f7350412c612088285f5ac8b7a3a6855bdb9
fili4 <filmon.e@yahoo.com>
2024-02-27 15:27:50 +0100

    lite style p[ quizz page/admin

--- src/pages/adminPage.js
+++ src/pages/adminPage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AdminPage() {
   return (
     <main>
-      <h1>Admin Page</h1>
+      <h1 className="p-4 grid justify-center items-center bg-slate-500 rounded-md m-4">Admin Page</h1>
     </main>
   );
 }
