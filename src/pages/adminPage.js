f074b62339634b59ecfc33e73d1ef06db510b0a4
fili4 <filmon.e@yahoo.com>
2024-02-29 17:32:55 +0100

    how it look

--- src/pages/adminPage.js
+++ src/pages/adminPage.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function AdminPage() {
   return (
-    <main>
-      <h1 className="p-4 grid justify-center items-center bg-slate-500 rounded-md m-4">Admin Page</h1>
+    <main className="grid grid-cols-1 gap-5 justify-center items-center">
+      <h1 className="p-4 grid justify-center items-center bg-slate-500 rounded-md m-4">
+        Admin Page
+      </h1>
+
+    
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,32 @@ const inter = Inter({ subsets: ["latin"] }); */
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="p-4 grid justify-center items-center bg-slate-500 rounded-md m-4">
-        Quiz Page
-      </h1>
+    <main className=" grid h-[771px] justify-around   bg-slate-700 ">
+      <div className="bg-slate-300 grid ">
+        <div className=" grid  max-w-xl m-6 ">
+          <h4 className="bg-slate-400  items-center  grid p-4 rounded-3xl text-center">
+            gjernerjgn erjgoerg rjehgbjergor jrngerngorngoerngrognoer
+            rgornegoierng hbsdjsjdfsdfj sdhbfsbdfjbf gjernerjgn erjgoerg
+            rjehgbjergor jrngerngorngoerngrognoer rgornegoierng hbsdjsjdfsdfj
+            sdhbfsbdfjbf gjernerjgn erjgoerg rjehgbjergor
+            jrngerngorngoerngrognoer rgornegoierng hbsdjsjdfsdfj sdhbfsbdfjbf
+          </h4>
+        </div>
+        <div className=" grid  bg-slate-200  grid-rows-2 justify-self-center gap-x-24 p-16 rounded-t-3xl  items-center  grid-cols-2">
+          <h3 className="bg-slate-400 justify-self-center grid p-4 rounded-3xl m-1 w-fit font-bold items-center">
+            A: Svar på frågan
+          </h3>
+          <h3 className="bg-slate-400 justify-self-center grid p-4 rounded-3xl m-1 w-fit font-bold items-center">
+            B: Svar på frågan
+          </h3>
+          <h3 className="bg-slate-400 justify-self-center grid p-4 rounded-3xl m-1 w-fit font-bold items-center">
+            C: Svar på frågan
+          </h3>
+          <h3 className="bg-slate-400 justify-self-center grid p-4 rounded-3xl m-1 w-fit font-bold items-center">
+            D: Svar på frågan
+          </h3>
+        </div>
+      </div>
     </main>
   );
 }
