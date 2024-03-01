32fe2d58c37d717e56a3ba519ab7f45f03337ca7
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-03-01 09:15:10 +0100

    Context Read/Add/Remove Ready

--- src/utils/context/contextProvider.js
+++ src/utils/context/contextProvider.js
@@ -1,6 +1,6 @@
-import { createContext, useContext, useReducer } from 'react'
+import { createContext, useContext, useReducer } from "react";
 //1 Create Context
-const QuestionsContext = createContext()
+const QuestionsContext = createContext();
 
 //3 Initial state reducer obj
 
@@ -16,38 +16,42 @@ const initialState = {
   questions: [
     {
       id: 1,
-      question: 'Question 1',
-      answer: 'Answer 1',
-      wrongAnswers: ['w1', 'w2', 'w3'],
+      question: "Question 1",
+      answer: "Answer 1",
+      wrongAnswers: ["w1", "w2", "w3"],
       points: 2,
     },
     {
       id: 2,
-      question: 'Question 2',
-      answer: 'Answer 2',
-      wrongAnswers: ['w1', 'w2', 'w3', 'w4'],
+      question: "Question 2",
+      answer: "Answer 2",
+      wrongAnswers: ["w1", "w2", "w3", "w4"],
       points: 5,
     },
   ],
-}
+};
 
 //4 Create reducer function dispatcher
-
 function questionsReducer(state, action) {
   switch (action.type) {
-    case 'ADD_QUESTION':
-      const newQuestion = action.payload
-      return [...state, newQuestion] //Works
+    case "ADD_QUESTION":
+      const newQuestion = action.payload;
+      return { ...state, questions: [...state.questions, newQuestion] };
 
-    case 'REMOVE_QUESTION': {
-      const questionId = action.payload
-      return (state = state.filter((question) => question.id !== questionId)) //Works
+    case "REMOVE_QUESTION": {
+      const questionId = action.payload;
+      return {
+        ...state,
+        questions: state.questions.filter(
+          (question) => question.id !== questionId
+        ),
+      };
     }
-    case 'UPDATE_QUESTION': {
+    case "UPDATE_QUESTION": {
       //TODO
     }
     default: {
-      throw Error('Unknown action: ' + action.type)
+      throw Error("Unknown action: " + action.type);
     }
   }
 }
@@ -55,15 +59,15 @@ function questionsReducer(state, action) {
 //2 Define Provider
 export const QuestionsProvider = ({ children }) => {
   //3 declare reducer  //5 pass reducers and initial states
-  const [state, dispatch] = useReducer(questionsReducer, initialState)
-  console.log(state)
+  const [state, dispatch] = useReducer(questionsReducer, initialState);
+
   //6 return the wrapper parent Context JSX.
   return (
     <QuestionsContext.Provider value={{ state, dispatch }}>
       {children}
     </QuestionsContext.Provider>
-  )
-}
+  );
+};
 
 //7 comprimiendo el código aun mas
-export const useQuestionsContext = () => useContext(QuestionsContext)
+export const useQuestionsContext = () => useContext(QuestionsContext);
