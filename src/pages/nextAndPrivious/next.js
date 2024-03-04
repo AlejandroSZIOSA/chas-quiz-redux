f611e0b2f8dc2bb641cc10e6b2cf896826324e38
fili4 <filmon.e@yahoo.com>
2024-03-04 15:31:22 +0100

    deleted next comp

--- src/pages/nextAndPrivious/next.js
+++
@@ -1,8 +0,0 @@
-export default function Next() {
-  return (
-    <>
-      <button className="bg-slate-600
-            font-extrabold text-cyan-50 hover:bg-slate-500">Next Question</button>
-    </>
-  );
-}
